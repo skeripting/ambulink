765530c2f33f519585d06ebfea86fbbeacbd6270
kusha <kt1500@rit.edu>
2024-02-25 10:07:38 -0500

    Added map functionality

--- Desktop/BrickHack/ambulink/src/App.js
+++ Desktop/BrickHack/ambulink/src/App.js
@@ -5,6 +5,8 @@ import AmbulanceLogin from "./pages/AmbulanceLogin/AmbulanceLogin";
 import PatientLogin from "./pages/PatientLogin/PatientLogin";
 import WelcomeDispatcher from "./pages/WelcomeDispatcher/WelcomeDispatcher";
 import AmbulanceAwaitingDispatch from "./pages/AmbulanceAwaitingDispatch/AmbulanceAwaitingDispatch";
+import WelcomePatient from "./pages/WelcomePatient/WelcomePatient";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
@@ -23,6 +25,10 @@ function App() {
           <Route
             path="/welcome-dispatcher"
             element={<WelcomeDispatcher></WelcomeDispatcher>}
+          />
+          <Route
+            path="/welcome-patient"
+            element={<WelcomePatient></WelcomePatient>}
           />
           <Route
             path="/ambulance-awaiting-dispatch"
--- Desktop/BrickHack/ambulink/src/pages/AmbulanceAwaitingDispatch/AmbulanceAwaitingDispatch.jsx
+++ Desktop/BrickHack/ambulink/src/pages/AmbulanceAwaitingDispatch/AmbulanceAwaitingDispatch.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./AmbulanceAwaitingDispatch.scss";
 import GradientBackground from "../../components/GradientBackground/GradientBackground";
 import FlipbookLayout from "../../components/FlipbookLayout/FlipbookLayout";
+import Map from "../../components/Map/Map";
+import { Helmet } from "react-helmet";
 
 function AmbulanceAwaitingDispatch() {
   const AWAITING_DISPATCH_STATE = 0;
@@ -29,7 +31,24 @@ function AmbulanceAwaitingDispatch() {
         )}
         {state == SHOWING_MAP_STATE && (
           <FlipbookLayout
-            left={<img src="/ambulance.png"></img>}
+            left={
+              <div className="map-container">
+                <Helmet>
+                  <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+                    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+                    crossorigin=""
+                  />
+                  <script
+                    src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+                    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+                    crossorigin=""
+                  ></script>
+                </Helmet>
+                <Map></Map>
+              </div>
+            }
             right={
               <div className="ambulance-right">
                 <p>Go to</p>
